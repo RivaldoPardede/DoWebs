fffc30768a2749053f6293c6acb6de1e6dda135b
RivaldoPardede <rivaldopardede3@gmail.com>
2023-11-13 21:03:11 +0700

    DoWebs launching

---
+++ dist/js/script.js
@@ -0,0 +1,67 @@
+// navbar fixed
+window.onscroll = () => {
+    const header = document.querySelector("header");
+    const fixedNav = header.offsetTop;
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add("navbar-fixed");
+    } else {
+        header.classList.remove("navbar-fixed");
+    }
+};
+
+// Hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hamburger-active");
+    navMenu.classList.toggle("hidden");
+});
+
+if (
+    localStorage.getItem("color-theme") === "dark" ||
+    (!("color-theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+    document.documentElement.classList.add("dark");
+} else {
+    document.documentElement.classList.remove("dark");
+}
+
+const themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+const themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+
+if (
+    localStorage.getItem("color-theme") === "dark" ||
+    (!("color-theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+    themeToggleLightIcon.classList.remove("hidden");
+} else {
+    themeToggleDarkIcon.classList.remove("hidden");
+}
+
+const themeToggleBtn = document.getElementById("theme-toggle");
+
+themeToggleBtn.addEventListener("click", function () {
+    themeToggleDarkIcon.classList.toggle("hidden");
+    themeToggleLightIcon.classList.toggle("hidden");
+
+    if (localStorage.getItem("color-theme")) {
+        if (localStorage.getItem("color-theme") === "light") {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("color-theme", "dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("color-theme", "light");
+        }
+    } else {
+        if (document.documentElement.classList.contains("dark")) {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("color-theme", "light");
+        } else {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("color-theme", "dark");
+        }
+    }
+});
---
+++ tailwind.config.js
@@ -0,0 +1,81 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["index.html"],
+    darkMode: "class",
+    theme: {
+        container: {
+            center: true,
+            padding: "16px",
+        },
+        extend: {
+            shadows: {
+                white: "0 1px 2px 0 rgba(255, 255, 255, 0.05)",
+            },
+            colors: {
+                dark: "#0f172a",
+                primary: {
+                    50: "#f8fafc",
+                    100: "#f1f5f9",
+                    200: "#e2e8f0",
+                    300: "#cbd5e1",
+                    400: "#94a3b8",
+                    500: "#64748b",
+                    600: "#475569",
+                    700: "#334155",
+                    800: "#1e293b",
+                    900: "#0f172a",
+                    950: "#020617",
+                },
+            },
+            fontFamily: {
+                poppins: ["Poppins"],
+                sans: [
+                    "Inter",
+                    "ui-sans-serif",
+                    "system-ui",
+                    "-apple-system",
+                    "system-ui",
+                    "Segoe UI",
+                    "Roboto",
+                    "Helvetica Neue",
+                    "Arial",
+                    "Noto Sans",
+                    "sans-serif",
+                    "Apple Color Emoji",
+                    "Segoe UI Emoji",
+                    "Segoe UI Symbol",
+                    "Noto Color Emoji",
+                ],
+            },
+        },
+        animation: {
+            typewriter: "typewriter 3s steps(17) forwards",
+            caret: "typewriter 3s steps(17) forwards, blink 1s steps(17) infinite 3s",
+        },
+        keyframes: {
+            typewriter: {
+                to: {
+                    left: "100%",
+                },
+            },
+            blink: {
+                "0%": {
+                    opacity: "0",
+                },
+                "0.1%": {
+                    opacity: "1",
+                },
+                "50%": {
+                    opacity: "1",
+                },
+                "50.1%": {
+                    opacity: "0",
+                },
+                "100%": {
+                    opacity: "0",
+                },
+            },
+        },
+    },
+    plugins: [require("flowbite/plugin")],
+};
